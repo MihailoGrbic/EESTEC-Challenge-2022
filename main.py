772324ad07fa9c93f1707c710846666d3c8fd705
Mihailo_Grbic <mihailogrbic99@gmail.com>
2022-03-19 18:23:42 +0100

    Minor changes

--- main.py
+++ main.py
@@ -14,13 +14,11 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTr
 from sklearn.model_selection import KFold, GroupKFold
 import pickle
 
-from util import lithology_key
+from util import fix_dataset
 
 SEED = 31415
 N_FOLDS = 8
 
-LR = 0.05
-N_ESTIMATORS = 50
 LOAD_MODEL = False
 
 Pkl_path = 'checkpoints/RDF1.pkl' 
@@ -36,6 +34,7 @@ MAPPING = {
 }
 
 train_dataset = pd.read_csv('data/Train-dataset.csv')
+# train_dataset = fix_dataset(train_dataset)
 
 X = train_dataset[['MD','GR', 'RT', 'DEN', 'CN','DEPOSITIONAL_ENVIRONMENT']]
 ct = ColumnTransformer([
@@ -64,15 +63,21 @@ else:
     kf = GroupKFold(n_splits=N_FOLDS)
     groups = train_dataset['WELL']
     groups = groups.replace(['Well-1', 'Well-2', 'Well-3'], 'Well-4')
+    # groups = groups.replace(['Well-01', 'Well-02', 'Well-03'], 'Well-04')
+    # group_list = ['Well-04', 'Well-05', 'Well-06', 'Well-07', 'Well-08', 'Well-09', 'Well-10', 'Well-11']
+    # print(groups)
 
     f1 = np.zeros((N_FOLDS))
+    val_well = []
     for i, (train_index, val_index) in enumerate(kf.split(X, groups=groups)):
         
         X_train, X_val = X[train_index], X[val_index]
         y_train, y_val = y[train_index], y[val_index]
 
-        model = ExtraTreesClassifier(n_estimators=500, min_samples_split=10, random_state=SEED, max_features='log2')
-        #model = AdaBoostClassifier(n_estimators=N_ESTIMATORS, learning_rate=LR, random_state=SEED)
+        #print(train_index)
+
+        model = ExtraTreesClassifier(n_estimators=100, min_samples_split=10, random_state=SEED, max_features='log2')
+        #model = AdaBoostClassifier(n_estimators=50, learning_rate=0.05, random_state=SEED)
 
         model.fit(X_train, y_train)
         yhat = model.predict(X_val)
@@ -80,7 +85,10 @@ else:
         models.append(model)
         f1[i] = metrics.f1_score(y_val, yhat, average='micro')
 
+        val_well.append(list(train_dataset.iloc[val_index]['WELL'].unique()))
+
     print(f1)
+    print(val_well)
     # Save the model to file in the current working directory
     with open(Pkl_path, 'wb') as file:  
         pickle.dump(models, file)
--- util.py
+++ util.py
@@ -15,6 +15,20 @@ def show_conf_matrix(y_test, y_pred, classes):
     
     plt.show()
 
+def fix_dataset(dataset):
+    dataset = dataset.replace('Well-1', 'Well-01')
+    dataset = dataset.replace('Well-2', 'Well-02')
+    dataset = dataset.replace('Well-3', 'Well-03')
+    dataset = dataset.replace('Well-4', 'Well-04')
+    dataset = dataset.replace('Well-5', 'Well-05')
+    dataset = dataset.replace('Well-6', 'Well-06')
+    dataset = dataset.replace('Well-7', 'Well-07')
+    dataset = dataset.replace('Well-8', 'Well-08')
+    dataset = dataset.replace('Well-9', 'Well-09')
+    dataset = dataset.sort_values('WELL')
+    print(dataset)
+    return dataset
+
 lithology_key = {100: 'Clay',
                  200: 'Siltstone/Loess',
                  300: 'Marl',
