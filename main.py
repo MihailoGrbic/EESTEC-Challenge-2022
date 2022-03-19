af6f563cbb30b4140a7fbec632a7bbe5baab195d
Mihailo_Grbic <mihailogrbic99@gmail.com>
2022-03-19 19:19:57 +0100

    Minor changes

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ from sklearn.compose import ColumnTransformer
 from sklearn import metrics
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.model_selection import KFold, GroupKFold
 import pickle
 
 from util import fix_dataset
 
 SEED = 31415
-N_FOLDS = 8
+N_FOLDS = 5
 
 LOAD_MODEL = False
 
-Pkl_path = 'checkpoints/RDF1.pkl' 
+Pkl_path = 'checkpoints/ADA1.pkl' 
 
 train_dataset = pd.read_csv('data/Train-dataset.csv')
 test_dataset = pd.read_csv('data/Test-dataset.csv')
@@ -34,7 +34,16 @@ MAPPING = {
 }
 
 train_dataset = pd.read_csv('data/Train-dataset.csv')
-# train_dataset = fix_dataset(train_dataset)
+
+
+train_dataset = train_dataset[train_dataset['WELL'] != 'Well-1']
+train_dataset = train_dataset[train_dataset['WELL'] != 'Well-2']
+train_dataset = train_dataset[train_dataset['WELL'] != 'Well-3']
+train_dataset = train_dataset[train_dataset['WELL'] != 'Well-4']
+train_dataset = train_dataset[train_dataset['WELL'] != 'Well-5']
+train_dataset = train_dataset[train_dataset['WELL'] != 'Well-9']
+# print(train_dataset['WELL'].unique())
+train_dataset = train_dataset.reset_index()
 
 X = train_dataset[['MD','GR', 'RT', 'DEN', 'CN','DEPOSITIONAL_ENVIRONMENT']]
 ct = ColumnTransformer([
@@ -63,9 +72,6 @@ else:
     kf = GroupKFold(n_splits=N_FOLDS)
     groups = train_dataset['WELL']
     groups = groups.replace(['Well-1', 'Well-2', 'Well-3'], 'Well-4')
-    # groups = groups.replace(['Well-01', 'Well-02', 'Well-03'], 'Well-04')
-    # group_list = ['Well-04', 'Well-05', 'Well-06', 'Well-07', 'Well-08', 'Well-09', 'Well-10', 'Well-11']
-    # print(groups)
 
     f1 = np.zeros((N_FOLDS))
     val_well = []
@@ -74,10 +80,10 @@ else:
         X_train, X_val = X[train_index], X[val_index]
         y_train, y_val = y[train_index], y[val_index]
 
-        #print(train_index)
-
-        model = ExtraTreesClassifier(n_estimators=100, min_samples_split=10, random_state=SEED, max_features='log2')
-        #model = AdaBoostClassifier(n_estimators=50, learning_rate=0.05, random_state=SEED)
+        # model = RandomForestClassifier(min_samples_split=29, random_state=SEED, max_features='log2')
+        # model = AdaBoostClassifier(n_estimators=100, learning_rate=0.1, random_state=SEED)
+        model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, random_state=SEED)
+        
 
         model.fit(X_train, y_train)
         yhat = model.predict(X_val)
